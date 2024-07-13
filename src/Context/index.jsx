74fa568b123127489ffbd455981803c3835ba20d
Victor-OT <crowbreaker96@hotmail.com>
2024-07-12 20:13:25 -0600

    Añadido correcto del componente Context

---
+++ src/Context/index.jsx
@@ -0,0 +1,75 @@
+import { createContext, useState, useEffect } from "react";
+
+const shoppiContext = createContext()
+
+function ShoppiContextProvider ({children}) {
+    const [userInput , setUserInput] = useState('')
+
+    //Get Data
+    const apiUrl = 'https://fakestoreapi.com/products/'
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch(apiUrl)
+        .then(response => response.json())
+        .then(response => {
+            setData(response)
+            setFilteredProducts(response)
+        })
+    }, [])
+
+    //Filter Products
+    const [filteredProducts, setFilteredProducts] = useState(null)
+
+    //Filter By Title
+    const filterProductsByTitle = () => {
+        const filterByTitle = data.filter(product => product.title.toLocaleLowerCase().includes(userInput))
+        setFilteredProducts(filterByTitle)
+    }
+
+    //Filter By Category
+    const [category, setCategory] = useState('')
+
+    const filterProductsByCategory = () => {
+        const filterByCategory = data.filter(product => product.category === category)
+        setFilteredProducts(filterByCategory)
+        return filterByCategory
+    }
+
+
+    useEffect(() => {
+        if (userInput === '' || category === '') {
+            setFilteredProducts(data)
+        }
+        if(userInput != '') {
+            filterProductsByTitle()
+        }
+
+        if (category != '') {
+            filterProductsByCategory()
+        }
+
+        if (category != '' && userInput != ''){
+            const doubleFilter = filterProductsByCategory().filter(product => product.title.toLocaleLowerCase().includes(userInput))
+            setFilteredProducts(doubleFilter)
+        }
+        
+    }, [userInput, category])
+
+    return (
+        <shoppiContext.Provider value={{
+            data,
+            setData,
+            filteredProducts,
+            setFilteredProducts,
+            userInput,
+            setUserInput,
+            category,
+            setCategory,
+        }}>
+            {children}
+        </shoppiContext.Provider>
+    )
+} 
+
+export {shoppiContext, ShoppiContextProvider}
