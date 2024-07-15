ef4b2948a1ce28d87a5f7fd61467bee3d5552b05
Victor-OT <crowbreaker96@hotmail.com>
2024-07-15 14:45:24 -0600

    Se desarrolló la lógica de pintar una CardOrder por cada elemento del array orders, de momento cada card aún tiene indormación pintada

--- src/Components/CardOrder/index.jsx
+++ src/Components/CardOrder/index.jsx
@@ -1,11 +1,11 @@
 import './CardOrder.css'
 
-function CardOrder () {
+function CardOrder (data) {
     return (
         <div className='order-card-container'>
             <div className='order-info'>
                 <p className='order-date'>15/07/2024</p>
-                <p>Arcicles: 4</p>
+                <p>Arcicles: {data.articles}</p>
             </div>
             <p className='total-order'>$109.99</p>
         </div>
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,15 +1,22 @@
+import { useContext } from "react"
+import { shoppiContext } from '../../Context'
 import { Layout } from "../../Components/Layout"
 import { CardOrder } from "../../Components/CardOrder"
 import './MyOrders.css'
 
 function MyOrders () {
+    const context = useContext(shoppiContext)
+
     return (
         <Layout>
             <h2>My Orders</h2>
             <div className='orders-container'>
-                <CardOrder />
-                <CardOrder />
-                <CardOrder />
+                {
+                    context.orders?.map(order => (
+                        <CardOrder
+                        articles={order.length}/>
+                    ))
+                }
             </div>
         </Layout>
     )
